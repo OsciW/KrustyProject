4d800bacba0ad8a7e4c377b6847ca46affb66136
osciw <oscarwerneman@gmail.com>
2016-03-30 18:17:28 +0200

    recipe with links

--- krusty_php/stocks.php
+++ krusty_php/stocks.php
@@ -5,28 +5,44 @@
 	$db = $_SESSION['db'];
 	$userId = $_SESSION['userId'];
 	$userType = $_SESSION['userType'];
+
+	  $db->openConnection();
+  $Recipies = $db->getRecipe();
+  $db->closeConnection();
 ?>
 
 
 <html>
-<head>
-<title>Krusty Kookies - <?php print $userType ?> </title>
-</head>
-<body>
+	<head>
+		<title>Krusty Kookies - <?php print $userType ?> </title>
+	</head>
+	<body>
 
-<h1 align="center"><?php print $userId ?>  <?php print $userType ?></h1>
+		<h1 align="center"> Stocks</h1>
+
+		<h1>Recipes</h1>
 
 
-<p>
-<form method=get action="createRecipe.php">
-    <input type=submit value="Manage recipies" >
-  </form>
+		<table id="materialtable">  
+  		<?php foreach($Recipies as $recipe){ ?>
+  			<tr> 
+ 
+    		<td> <a href="checkRecipe.php?recipe=<?php print $recipe?>">  <?php print $recipe?> </a></td>
 
-<p>
+ 			</tr>
+  		<?php } ?>
 
-<form method=get action="index.html">
-    <input type=submit value="log out" >
-  </form>
+		</table>
 
-</body>
+		<p class="text">
+  			<a href="createRecipe.php">Create Recipes</a>
+		</p>
+
+
+		<p>
+
+		<form method=get action="index.html">
+    		<input type=submit value="log out" >
+  		</form>
+	</body>
 </html>
