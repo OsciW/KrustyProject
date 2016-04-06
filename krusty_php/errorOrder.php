effdee39f3e90f3dc933e6757e5d805a52044cf5
hborism <henrik.borismoller@gmail.com>
2016-04-06 10:17:10 +0200

    just adding description of error to include number of cookies must be larger than 0

--- krusty_php/errorOrder.php
+++ krusty_php/errorOrder.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "ERROR. Go back using the browsers back button and please make sure the entries are in the correct format";
+echo "ERROR. Go back using the browsers back button and please make sure the entries are in the correct format and make sure that at least one cookie is ordered";
 
 ?>
