3b596abbd11e1dd4de6d4703fd31fe8f4d5fbc66
osciw <oscarwerneman@gmail.com>
2016-04-08 00:54:20 +0200

    customer added

--- krusty_php/orderprocessing.php
+++ krusty_php/orderprocessing.php
@@ -6,15 +6,16 @@
 	$userId = $_SESSION['userId'];
 	$userType = $_SESSION['userType'];
 
+	if($userType == 'Customer') {
+		$customerName = $_SESSION['customerName'];
+	} else {
+		$customerName = $_REQUEST['customerName'];
+	}
+
 	$recipes = $_SESSION['recipes'];
 	$date=$_POST['deliveryDate'];
 	$time=$_POST['deliveryTime'];
 
-	/*
-	if($date<=date("y-m-d")){
-		header("Location: errorOrder.php");
-	}
-	*/
 
 
 	unset($_POST['deliveryDate']);
@@ -26,27 +27,30 @@
 	$i=0;
 	foreach ($_POST as $key => $value) {
 		$no_cookies=$no_cookies+$value;
+
 		$specs[$i]=array($recipes[$i], $value);
   		$i++;
 	}
 
-	if($no_cookies==0) {
+
+	/*if($no_cookies==0) {
 		header("Location: errorOrder.php");
-	}
+	}*/
 
 	$db->openConnection();
-	$customerAddress= $db->getCustomerAddress($userId);
-	$orderSpec=$db->placeOrder($userId, $time, $date, $specs);
+
+	$orderSpec=$db->placeOrder($customerName, $time, $date, $specs);
   	$db->closeConnection();
 
-  	if($orderSpec[0] == NULL){
+  	/*if($orderSpec[0] == NULL){
+  	
   		header("Location: errorOrder.php");
-  	}
+  	}*/
 
 
   	echo 'Order confirmation </br></br>';
 	echo 'User: '.$userId."</br>";
-	echo 'Delivery address: '.$customerAddress."</br>";
+	echo 'Customer: '.$customerName."</br>";
 
 	echo 'Delivery date: ';
 	echo $date."</br>";
@@ -62,20 +66,6 @@
 	for($i=1; $i<count($orderSpec); $i++) {
 		echo $orderSpec[$i][0].": ".$orderSpec[$i][1]."<br />";
 	}
-
-/*
-	foreach ($orderSpec as $item) {
-		echo $item[0].": {$item[1]}<br />";
-	}
-
-*/
-	/*
-	$i=0;
-	foreach ($_POST as $key => $value) {
-		echo $recipes[$i].": {$value}<br />";
-  		$i++;
-	}
-	*/
 
 
 ?>
