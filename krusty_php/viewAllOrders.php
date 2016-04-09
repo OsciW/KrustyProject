538a6d2589a467fa978c6c893c37f0a7e9c67cf3
osciw <oscarwerneman@gmail.com>
2016-04-09 09:24:22 +0200

    borde funka nu

--- krusty_php/viewAllOrders.php
+++ krusty_php/viewAllOrders.php
@@ -6,22 +6,23 @@
 	$userId = $_SESSION['userId'];
 	$userType = $_SESSION['userType'];
   $action = $_GET['action'];
+  $customerName = $_GET['customerName'];
 
 	$db->openConnection();
-
+  $customer = $db->getCompanies();
 	$orders = $db->getOrders();
-  
-  if($action == "Orders") {
-    $pallets = $db->getAllOrders();
+  if(!$customerName) {
+    if($action == "Orders") {
+      $pallets = $db->getAllOrders();
+    } else {
+      $pallets = $db->getPalletOrders();
+    }
   } else {
-    
-    $pallets = $db->getPalletOrders();
+    $pallets = $db->getPalletOrdersForC($customerName);
   }
-
+  
 	$db->closeConnection();
-
-  header("viewAllOrders.php");
-?>
+  ?>
 
 
 <html>
@@ -29,6 +30,10 @@
 <title>Krusty Kookies</title>
 </head>
 <body>
+
+
+  
+
 
 
 
@@ -40,20 +45,48 @@
       <form method=get action="viewAllOrders.php">
         <input name = "action" type=submit value="Pallets used" >
       </form>
+        <form method get action "viewAllOrders.php">
+<tr>
+    <td align="center"><select name="customerName">
+     <option name="sumthing" value="">All companies</option>
+    <?php
+  foreach ($customer as $name ) {
+?>
+      <option name="customerName" value="<?php print $name ?>"><?php print $name?></option>
+    
+<?php
+  }
+?>
+ </select></td>
+ <input type=submit value="check" >
 
 
-
+<?php if($action =='Orders') { ?>
 <h3>Orders</h3>
+<?php } else { ?>
+
+  <h3>Delivered Pallets </h3>
+<?php } ?>
     <table id="palletTable" style="border-spacing: 20px">
       <tr>
+      <?php  if($action =='Orders') { ?>
         <td style="background-color: #FFF"><b>Order Id</b></td>
         <td style="background-color: #FFF"><b>Customer name</b></td>
         <td style="background-color: #FFF"><b>Created Date</b></td>
         <td style="background-color: #FFF"><b>Delivery date</b></td>
+        <td style="background-color: #FFF"><b>Status</b></td>
         <td style="background-color: #FFF"><b></b></td>
         <td style="background-color: #FFF"><b></b></td>
-        <td style="background-color: #FFF"><b></b></td>
-  
+  <? } else { ?>
+
+       <td style="background-color: #FFF"><b>Pallet Id</b></td>
+        <td style="background-color: #FFF"><b>Created Date</b></td>
+        <td style="background-color: #FFF"><b>Recipe name</b></td>
+        <td style="background-color: #FFF"><b>Order Id</b></td>
+        <td style="background-color: #FFF"><b>Customer name</b></td>
+        <td style="background-color: #FFF"><b>Delivery date</b></td>
+
+ <?php  } ?>
 
       </tr>
       <?php
@@ -91,19 +124,18 @@
       ?>
     </table>
 
+ </tr>
+ </form>
 
-
-
-
-    <form method=get action="ordersDeliver.php">
-        <input type=submit value="Back" >
+ <form method=get action="ordersDeliver.php">
+        <input type=submit value="back" >
       </form>
-
-
+      
      <form method=get action="index.html">
         <input type=submit value="log out" >
       </form>
 
+        
 
 
 
